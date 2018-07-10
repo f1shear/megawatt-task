8633a3726f3b4cee8228934c00c72bdbc35321cb
Ravi Simon <simonravi@yahoo.com>
2018-07-10 16:59:08 +0530

    added model and views tests

--- apps/sites/tests.py
+++ apps/sites/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from decimal import Decimal
+from .models import SiteModel, SiteDataModel
+
+
+class SiteTest(TestCase):
+
+    def setUp(self):
+
+        self.site = SiteModel.objects.create(id=1, name='XYZ Site')
+
+        self.site_data = SiteDataModel.objects.create(
+            site=self.site,
+            a_value=Decimal(5.0),
+            b_value=Decimal(5.0))
+
+    def test_site_model(self):
+        self.assertEqual(str(self.site), "Site: XYZ Site")
+        self.assertEqual(self.site.name, "XYZ Site")
+
+    def test_site_data_model(self):
+        self.assertEqual(self.site_data.a_value, Decimal(5.0))
+        self.assertEqual(self.site_data.b_value, Decimal(5.0))
+
+    def test_site_list_page(self):
+        response = self.client.get('/sites/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_site_detail_page(self):
+        response = self.client.get('/sites/%s/' % self.site.id)
+        self.assertEqual(response.status_code, 200)
+
+    def test_site_summary_sum_page(self):
+        response = self.client.get('/summary/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_site_summary_avg_page(self):
+        response = self.client.get('/summary-average/')
+        self.assertEqual(response.status_code, 200)
